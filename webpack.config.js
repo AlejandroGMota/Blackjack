ecc75f66bb23af141a9cb734cdec39ecebb82965
Enrique <cmke14y@gmail.com>
2021-08-17 18:10:32 -0500

    setup inical 1.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtract= require('mini-css-extract-plugin');
 const CopyPlugin = require("copy-webpack-plugin")
+const CssMinimizer = require("css-minimizer-webpack-plugin")
+const Terser = require("terser-webpack-plugin")
 
 module.exports={
     
     mode: 'development',
+    output:{
+        clean:true,
+        filename: 'main.[contenthash].js'
+    },
     module: {
         rules:[
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
             {
                 test:/\.html$/i,
                 loader:'html-loader',
@@ -29,19 +45,19 @@ module.exports={
             },
         ]
     },
-        plugins:[
-            new HtmlWebPackPlugin({
-                template: './src/index.html',
-                filename: './index.html'
-            }),
-            new MiniCssExtract({
-                filename: 'styles.css',
-                ignoreOrder:false,
-            }),
-            new CopyPlugin({
-                patterns:[
-                    {from:'src/cartas/', to:'cartas/'}
-                ]
-            })
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtract({
+            filename: 'styles.css',
+            ignoreOrder:false,
+        }),
+        new CopyPlugin({
+            patterns:[
+                {from:'src/cartas/', to:'cartas/'}
+            ]
+        })
     ]
 }
